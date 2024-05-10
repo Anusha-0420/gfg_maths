196061a4fa9cb931f55770b228320b201871ec95
Anusha-0420 <saic3693@gmail.com>
2024-05-10 20:15:05 +0530

    Create DICE PROBLEM.py

---
+++ DICE PROBLEM.py
@@ -0,0 +1,29 @@
+You are given a cubic dice with 6 faces. All the individual faces have a number printed on them. The numbers are in the range of 1 to 6, like any ordinary dice. You will be provided with a face of this cube, your task is to guess the number on the opposite face of the cube.
+  code: 
+
+class Solution:
+    def oppositeFaceOfDice(self, N):
+    	#code here 
+        return 7-N
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.oppositeFaceOfDice(N)
+        print(ans)
+Explanation:
+1. The Solution class is defined, which presumably contains methods for solving specific problems.
+2. The oppositeFaceOfDice method takes two arguments: self (indicating that this method is part of a class) and an integer N.
+3. It calculates the value of the opposite face of a dice given the current face value N. The opposite face value is obtained by subtracting N from 7 (since the sum of opposite faces on a standard six-sided die is always 7).
+4. The method returns the calculated opposite face value.
+5. The code reads an integer t (presumably the number of test cases) and iterates through each test case:
+  a) For each test case, it reads an integer N.
+  b) An instance of the Solution class is created.
+  c) The oppositeFaceOfDice method is called with the input integer N.
+  d) The result (the value of the opposite face) is printed for each test case.
+However, there are a few things to note:
+
+The code assumes that the input format is as described (number of test cases followed by the integer values).
+It does not handle invalid inputs or edge cases.
